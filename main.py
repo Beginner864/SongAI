109aebd3d66b2e61c6a9500cfe5d5b84fe4cb432
눈사람 <noonma777@gmail.com>
2025-05-28 05:21:57 +0900

    아아아아

--- main.py
+++ main.py
@@ -79,11 +79,11 @@ def recommend(req: RecommendRequest):
     for i, sim in ranked:
         song = req.user_songs[i]
         print(f"  - ID {song.id:>3} | {sim:.4f} | {song.title}")
-        if sim >= 0.5:  # 0.5 이상인 곡만 필터링
+        if sim >= 0.2:  # 0.5 이상인 곡만 필터링
             candidates.append(song)
 
     if not candidates:
-        print("유사도 기준(0.5) 이상인 곡이 없음\n")
+        print("유사도 기준(0.2) 이상인 곡이 없음? 체감상 0.8인데?\n")
         return {"error": "추천 가능한 곡이 없습니다."}
 
     print("유사도 기준 통과 → 랜덤 추천 진행\n")
